2b5588e697c5fae013c6a341e086147ae6d7a465
Rostomi <williamwilson265@gmail.com>
2019-03-05 20:45:15 +0400

    Enter

--- js/main.js
+++ js/main.js
@@ -1,11 +1,1 @@
-$('.drop-4').click(function(){ 
-    $('.tech-ul-2').toggleClass('hide');
-});
-
-$('.drop-3').click(function(){ 
-    $('.tech-ul').toggleClass('hide');
-});
-
-$('.drop-5').click(function(){ 
-    $('.tech-ul-3').toggleClass('hide');
-});
+$(".sign-in").toggleClass("fa-user");
