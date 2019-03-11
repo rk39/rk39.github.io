bcce93b9082a27e9a754c105209574a011eb317c
Rostomi <williamwilson265@gmail.com>
2019-03-12 03:31:56 +0400

    Enter

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-$(".sign-in").toggleClass("fa-user");
+var masivi = [4,5,2,8,1,0,7]
+
+for ( var i = 1; i < masivi.length; i++){
+
+    function saxeli () {
+        var k = i;
+        setTimeout(() => {
+            console.log(k);
+        }, k * 500);
+    };
+    saxeli();
+}
---
+++ js/tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+for (var index = 0; index < masivi.length; index++)
