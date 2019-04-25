9771c2e9cb36e8ffef8ee2240b35efde698a3123
Rostomi <williamwilson265@gmail.com>
2019-04-25 13:15:46 +0400

    Site Build Stopped

--- js/main.js
+++ js/main.js
@@ -4,13 +4,21 @@
   
     // document.querySelector(".bar6").addEventListener("click", function(){
     //   var y = document.querySelector(".nav-menu-ul");
-    //   if (y.style.display === "block") {
-    //     y.style.display = "none";
+    //   if (y.style.display === "none") {
+    //     y.style.display = "flex";
     //     console.log("clicked kent")
     //   } else {
-    //     y.style.display = "block";
+    //     y.style.display = "none";
     //     y.style.backgroundColor = "red";
     //     console.log("clicked luw")
     //   }
     // })
   
+
+    var forAddClasses = ["flex-column"];
+    var forRemoveClasses = [""];
+
+    document.querySelector(".bar6").addEventListener("click", function clickChanger(){
+      var elem = document.querySelector(".nav-menu-ul");
+      elem.classList.toggle(...forAddClasses, ...forRemoveClasses);
+    })
