572e0bdeda8f0376d2457384894281f07d78b042
Rostomi <williamwilson265@gmail.com>
2019-04-30 17:17:09 +0400

    Update main.js
    Added fa-times ti navigation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 document.querySelector(".bar6").addEventListener("click", function(){
+    document.querySelector(".fa-bars").classList.toggle("fa-times");
     document.getElementById("hovered-div").classList.toggle("block");
 })
