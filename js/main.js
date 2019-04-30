45002affe30182094b0c25460ceeec61db482d25
Rostomi <williamwilson265@gmail.com>
2019-04-30 17:12:43 +0400

    Nav Dropdown Completes Without Animation and Transition

--- js/main.js
+++ js/main.js
@@ -1,24 +1,3 @@
-
-
-  // d-flex flex-nowrap justify-content-center d-sm-none d-md-none d-lg-flex d-xl-flex
-  
-    // document.querySelector(".bar6").addEventListener("click", function(){
-    //   var y = document.querySelector(".nav-menu-ul");
-    //   if (y.style.display === "none") {
-    //     y.style.display = "flex";
-    //     console.log("clicked kent")
-    //   } else {
-    //     y.style.display = "none";
-    //     y.style.backgroundColor = "red";
-    //     console.log("clicked luw")
-    //   }
-    // })
-  
-
-    var forAddClasses = ["flex-column"];
-    var forRemoveClasses = [""];
-
-    document.querySelector(".bar6").addEventListener("click", function clickChanger(){
-      var elem = document.querySelector(".nav-menu-ul");
-      elem.classList.toggle(...forAddClasses, ...forRemoveClasses);
-    })
+document.querySelector(".bar6").addEventListener("click", function(){
+    document.getElementById("hovered-div").classList.toggle("block");
+})
